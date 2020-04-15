f8c715bb453c7928e95b17c7c6ffb314a802386e
Adam Dierkens <Adam_Dierkens@intuit.com>
2020-04-15 00:50:05 -0700

    Use Configuration object from eyes.webdriver

--- plugins/applitools/src/types/applitools-selenium.d.ts
+++ plugins/applitools/src/types/applitools-selenium.d.ts
@@ -1,24 +1,4 @@
 declare module '@applitools/eyes-selenium' {
-  export class Configuration {
-    public stitchMode: 'CSS';
-
-    constructor();
-
-    getBranchName(): string;
-    setBranchName(name: string): void;
-    getBaselineBranchName(): string;
-    setBaselineBranchName(name: string): void;
-    getParentBranchName(): string;
-    setParentBranchName(name: string): void;
-    setAppName(name: string): void;
-    setTestName(name: string): void;
-    setApiKey(key: string): void;
-    setForceFullPageScreenshot(force: boolean): void;
-    setHideScrollbars(hide: boolean): void;
-
-    addBrowser(width: number, height: number, browserType: BrowserType): void;
-  }
-
   type BrowserType = 'CHROME';
   export const BrowserType: Record<string, BrowserType>;
 }
